dc3f2d44e4d53c0c5e769cce7bd6e0251467b5e6
rodrigociro <rodrigociro@hotmail.es>
2023-08-11 07:30:21 +0000

    test print

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,35 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("@actions/core");
-const deploy_1 = require("./actions/deploy");
-const actions_1 = require("./types/actions");
-let entrada1 = (0, core_1.getInput)("entrada1");
-let entrada2 = (0, core_1.getInput)("entrada2");
-(0, core_1.info)("Parametros de mi accion");
-//llamo a mi objeto y defino sus propiedades
-let request = new actions_1.ActionRequest();
-request.parametro_recibido1 = entrada1;
-request.parametro_recibido2 = entrada2;
-if (!request.isValid()) {
-    (0, core_1.setFailed)("Missing mandatory parameters " + request.errMessage);
-    throw new Error("Missing mandatory parameters " + request.errMessage);
-}
-(0, core_1.info)(`Executing helm-deploy for environment ${request.parametro_recibido1} and region ${request.parametro_recibido2}`);
-new deploy_1.DeployAction().execute(request).then(result => {
-    (0, core_1.info)("\u001b[32mEjecucion terminada");
-}).catch(err => {
-    (0, core_1.error)("Algo ha ido mal " + err.message);
-    (0, core_1.setFailed)("Algo ha ido mal");
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
 });
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(require("@actions/core"));
+//backgroud color
+var yellow = '\u001b[43m';
+var cyan = '\u001b[48;5;6m';
+var red = '\u001b[48;2;255;0;0m';
+//Foreground colors
+let magenta = '\u001b[35m';
+var cyanf = '\u001b[38;5;6m';
+var redf = '\u001b[38;2;255;0;0m';
+core.info(redf + "hola");
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,12 @@
-import  { getInput, info, error, setFailed } from '@actions/core'
-import { DeployAction } from './actions/deploy';
-import { ActionRequest } from './types/actions';
+import * as core from '@actions/core';
 
-let entrada1 : string = getInput("entrada1");
-let entrada2 : string = getInput("entrada2");
+//backgroud color
+var yellow: string ='\u001b[43m';
+var cyan: string ='\u001b[48;5;6m'
+var red: string = '\u001b[48;2;255;0;0m'
+//Foreground colors
+let magenta:string='\u001b[35m'
+var cyanf:string='\u001b[38;5;6m'
+var redf:string='\u001b[38;2;255;0;0m'
 
-info("Parametros de mi accion");
-//llamo a mi objeto y defino sus propiedades
-let request = new ActionRequest();
-request.parametro_recibido1 = entrada1;
-request.parametro_recibido2 = entrada2;
-
-if (!request.isValid()) {
-    setFailed("Missing mandatory parameters " + request.errMessage);
-    throw new Error("Missing mandatory parameters " + request.errMessage);
-}
-
-info(`Executing helm-deploy for environment ${request.parametro_recibido1} and region ${request.parametro_recibido2}`);
-new DeployAction().execute(request).then(result => {
-    info("\u001b[32mEjecucion terminada");
-}).catch(err => {
-    error("Algo ha ido mal " + err.message);
-    setFailed("Algo ha ido mal");
-});
+core.info(redf+"hola")
