75487fba941229732aea361c7cae30e3122f2533
rodrigociro <rodrigociro@hotmail.es>
2023-08-15 08:48:33 +0000

    test rest/api

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
 const core_1 = require("@actions/core");
 var url = 'https://dummy.restapiexample.com/api/v1/employees';
-var data = axios_1.default.get(url)
+axios_1.default.get(url)
     .then(function (response) {
-    (0, core_1.info)(response.data);
+    (0, core_1.info)("Contenido:\n" + JSON.stringify(response.data));
 });
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import axios, { AxiosResponse } from 'axios';
 import  { getInput, info, error, setFailed } from '@actions/core'
 
 var url = 'https://dummy.restapiexample.com/api/v1/employees'
-var data = axios.get(url)
+
+axios.get(url)
   .then(function (response){
-    info(response.data)
+    info("Contenido:\n"+JSON.stringify(response.data))
 })
+
