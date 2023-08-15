9dd8d0cd8a7a91445cf92a4100cc68c91f19f1b4
rodrigociro <rodrigociro@hotmail.es>
2023-08-15 09:44:19 +0000

    test rest/api

--- dist/index.js
+++ dist/index.js
@@ -5,21 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
 const core_1 = require("@actions/core");
+var method = (0, core_1.getInput)("method");
+if (method == "") {
+    method = 'GET';
+}
+var options = (0, core_1.getInput)("options");
 var BASE_URL_KEY = 'https://dummy.restapiexample.com';
 var API_VERSION_KEY = '/api/v1';
 var PATH = '/employees';
 var URL = BASE_URL_KEY.concat(API_VERSION_KEY, PATH);
-(0, core_1.info)(URL);
-axios_1.default.get(URL)
-    .then(function (response) {
-    evaluar(response);
-})
-    .catch(function (error) {
-    (0, core_1.info)("hola desde el error");
-})
-    .finally(function () {
-    (0, core_1.info)("hola desde finally");
-});
+getDataFromAction(URL, method);
+//function to show data from API request
 function evaluar(response) {
     var datos = response.data;
     var estado = response.status;
@@ -27,4 +23,28 @@ function evaluar(response) {
     var cabeceras = response.headers;
     var configuracion = response.config;
     (0, core_1.info)("datos:\n" + JSON.stringify(datos) + "respuesta:\n" + JSON.stringify(estado));
+}
+//main function
+function getDataFromAction(url, method, options) {
+    if (method == 'GET') {
+        axios_1.default.get(URL)
+            .then(function (response) {
+            evaluar(response);
+        })
+            .catch(function (error) {
+            error("Something wrong with get: " + error);
+        })
+            .finally(function () {
+            (0, core_1.info)("hola desde finally");
+        });
+    }
+    else if (method == 'POST') {
+        console.log("hi");
+    }
+    else if (method == 'PUT') {
+        console.log("hi");
+    }
+    else {
+        (0, core_1.error)("Wrong method value");
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,20 @@
 import axios, { AxiosResponse } from 'axios';
 import  { getInput, info, error, setFailed } from '@actions/core'
+import { isExpressionWithTypeArguments } from 'typescript';
 
+var method = getInput("method")
+if(method == ""){
+    method = 'GET'
+}
+var options = getInput("options")
 var BASE_URL_KEY = 'https://dummy.restapiexample.com'
 var API_VERSION_KEY = '/api/v1'
 var PATH = '/employees'
 var URL = BASE_URL_KEY.concat(API_VERSION_KEY,PATH)
-info(URL)
 
-axios.get(URL)
-  .then(function (response) {
-    evaluar(response);
-  })
-  .catch(function (error) {
-    info("hola desde el error")
-  })
-  .finally(function () {
-    info("hola desde finally")
-  });
+getDataFromAction(URL,method)
 
+//function to show data from API request
 function evaluar(response:AxiosResponse) {
     var datos = response.data
     var estado = response.status
@@ -26,4 +23,27 @@ function evaluar(response:AxiosResponse) {
     var configuracion = response.config
     info("datos:\n"+JSON.stringify(datos)+"respuesta:\n"+JSON.stringify(estado))
 }
+
+//main function
+function getDataFromAction(url:string,method:string,options?:string){
+    if(method == 'GET'){
+        axios.get(URL)
+            .then(function (response) {
+                evaluar(response);
+            })
+            .catch(function (error) {
+                error("Something wrong with get: "+error)
+            })
+            .finally(function () {
+                info("hola desde finally")
+            });
+    }else if(method== 'POST'){
+        console.log("hi")
+    }else if(method=='PUT'){
+        console.log("hi")
+    }else{
+        error("Wrong method value")
+    }
+}
+
 
