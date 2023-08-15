2331b8571f4a66b7a1630a1358a820a99593c707
rodrigociro <rodrigociro@hotmail.es>
2023-08-15 07:39:14 +0000

    test get method

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,8 @@
-import  { getInput, info, error, setFailed } from '@actions/core'
-import { DeployAction } from './actions/deploy';
-import { ActionRequest } from './types/actions';
+import * as core from '@actions/core';
+const { curly } = require('node-libcurl');
+const querystring = require('querystring');
+const { statusCode, data, headers } = curly.get('https://dummy.restapiexample.com/api/v1/employees')
 
-let entrada1 : string = getInput("entrada1");
-let entrada2 : string = getInput("entrada2");
-
-info("Parametros de mi accion");
-//llamo a mi objeto y defino sus propiedades
-let request = new ActionRequest();
-request.parametro_recibido1 = entrada1;
-request.parametro_recibido2 = entrada2;
-
-if (!request.isValid()) {
-    setFailed("Missing mandatory parameters " + request.errMessage);
-    throw new Error("Missing mandatory parameters " + request.errMessage);
-}
-
-info(`Executing helm-deploy for environment ${request.parametro_recibido1} and region ${request.parametro_recibido2}`);
-new DeployAction().execute(request).then(result => {
-    info("\u001b[32mEjecucion terminada");
-}).catch(err => {
-    error("Algo ha ido mal " + err.message);
-    setFailed("Algo ha ido mal");
-});
+core.info(data)
+core.info(statusCode)
+core.info(headers)
