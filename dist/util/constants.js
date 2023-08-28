f7dd3339482526553991f6ba8bc8d2c001ad6eef
rodrigociro <rodrigociro@hotmail.es>
2023-08-28 06:39:00 +0000

    test params from curl

--- dist/util/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DeployStrategy = void 0;
-class DeployStrategy {
-}
-exports.DeployStrategy = DeployStrategy;
-DeployStrategy.BLUEGREEN = ["bluegreen"];
-DeployStrategy.NONE = ["none", "default"];
